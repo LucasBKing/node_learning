2f71bc6eece895121cb4045ee353d7fef91574d9
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-22 10:13:14 -0300

    using consign to refactoring the routes

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,25 @@
-let express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            name: 'LucasBKing',
-            email: 'ldsbarbosa@inf.ufpel.edu.br',
-            id: 1
-        }]
+module.exports = (app) => {
+    app.get('/users', (req,res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'LucasBKing',
+                email: 'ldsbarbosa@inf.ufpel.edu.br',
+                id: 1
+            }]
+        });
     });
-});
-
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            name: 'admin',
-            email: 'admin@comp.admin',
-            id: 0
-        }]
+    
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'admin',
+                email: 'admin@comp.admin',
+                id: 0
+            }]
+        });
     });
-});
-
-module.exports = routes;
+};
