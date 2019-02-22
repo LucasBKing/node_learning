c96cc6c2a0e1ea18027d2a841454944f6d761005
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-02-22 10:49:28 -0300

    listing users from nedb

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,20 @@ let db = new NeDB({
 
 module.exports = (app) => {
     app.get('/users', (req,res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                name: 'LucasBKing',
-                email: 'ldsbarbosa@inf.ufpel.edu.br',
-                id: 1
-            }]
+
+        db.find({}).sort({name:1}).exec((err, users) => {
+            if (err) {
+                console.log(`Error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
         });
     });
     
